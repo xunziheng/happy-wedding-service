97988ab1608de24366c7ae3fc623ef19900f401d
xunziheng <xunziheng88@163.com>
2022-09-15 17:43:15 +0800

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import statusMonitorConfig from './config/monitor';
+import weixinConfig from './config/weixin';
 import { HttpModule } from '@nestjs/axios';
 import { WeChatModule } from 'nest-wechat';
-import { getConfig } from './main';
-
-const weixin: any = getConfig().get('weixin');
 
 @Module({
   imports: [
     UserModule,
-    WeChatModule.register({ appId: weixin.appId, secret: weixin.secret }),
+    WeChatModule.register({
+      appId: weixinConfig.appId,
+      secret: weixinConfig.secret,
+    }),
     HttpModule.register({
       timeout: 5000,
       maxRedirects: 5,
