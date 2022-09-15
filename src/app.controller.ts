a2dfac94f64f1b301c32db8c9c80ae82e298144a
xunziheng <xunziheng88@163.com>
2022-09-15 19:03:32 +0800

    weixin server

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { WeChatService } from 'nest-wechat';
+import weixinConfig from './config/weixin';
+import { MessageCrypto } from 'nest-wechat';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly weChatService: WeChatService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('weixinCheck')
+  weixinCheck(
+    @Query('signature') signature: string,
+    @Query('timestamp') timestamp: string,
+    @Query('nonce') nonce: string,
+    @Query('echostr') echostr: string,
+  ): string {
+    const token = weixinConfig.token;
+    const arr = [];
+    arr.push(token, timestamp, nonce);
+    arr.sort();
+    const str = arr.join('');
+    const sha1 = MessageCrypto.sha1(str);
+    if (sha1 === signature) {
+      return echostr;
+    }
+    return;
   }
 }
