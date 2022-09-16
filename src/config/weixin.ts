136078fb2716a516a24dea93f89b155b98b7cdfb
xunziheng <xunziheng88@163.com>
2022-09-16 09:15:13 +0800

    private config .ignore

--- src/config/weixin.ts
+++ src/config/weixin.ts
@@ -1,6 +1,7 @@
 export default {
-  appId: '',
-  secret: '',
-  token: '',
-  encodingAESKey: '',
+  appId: 'wx6550244a92015fa4',
+  secret: '300cbc433b15321c0a587e2ccf2321ba',
+  token: 'wxToken_JAZcCbKQjs',
+  encodingAESKey: 'oe7dgg8yYkOExn2l4xZa29VJHfgykeDMQH46A2hegXi',
+  redirectUri: 'local.xunziheng.top',
 };
